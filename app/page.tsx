2d9a9e3533486f61086234739ff777bc2cf4a20f
pero4l <lilpero03@gmail.com>
2026-02-14 00:49:30 +0100

    Added the contract folder

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
    <>
-      <h1 className="text-3xl font-bold text-blue-300 bg-blue-50 underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-blue-300 bg-blue-50 underline">Hello world!!</h1>
    </>
   );
 }
